a4d7a76008215a228828fd271ca80ac614965420
xWuWux <wojteklobo@gmail.com>
2019-09-29 08:39:29 +0200

    commit nr 1

--- Sprawdzian-1-WSB/Program.cs
+++ Sprawdzian-1-WSB/Program.cs
@@ -10,6 +10,20 @@ namespace Sprawdzian_1_WSB
     {
         static void Main(string[] args)
         {
+            int[] array1 = { 1, 3, 5, 7 };
+            List<int> list1 = new List<int>() { 1, 3, 5, 7 };
+
+            //
+            // Use Sum extension on their elements.
+            //
+            int sum1 = array1.Sum();
+            int sum2 = list1.Sum();
+
+            //
+            // Write results to screen.
+            //
+            Console.WriteLine(sum1);
+            Console.WriteLine(sum2);
         }
     }
 }
